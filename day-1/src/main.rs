f6aac939faa13378cbdaded4cd29fe7b6acc9a30
ArkhamCookie <git@mail.theuntitledproject.dev>
2025-12-01 17:31:03 -0600

    feat: messy part 1

--- day-1/src/main.rs
+++ day-1/src/main.rs
@@ -1,3 +1,60 @@
+use common::input_file;
+
+fn part_a(input: &str) -> i32 {
+	let mut position = 50;
+	let mut output = 0;
+
+	for line in input.lines() {
+		let line = line.trim();
+
+		if line.is_empty() {
+			continue;
+		}
+
+		let number: i32 = line[1..].parse().unwrap();
+		match line.as_bytes()[0] {
+			b'L' => position = (100 + position - number % 100) % 100,
+			b'R' => position = (position + number) % 100,
+			_ => unreachable!("unexpected instruction"),
+		}
+
+		if position == 0 {
+			output += 1;
+		}
+	}
+
+	output
+}
+
 fn main() {
-    println!("Hello, world!");
+	let input = input_file();
+
+	let part_a = part_a(&input);
+
+	println!("Part A: {}", part_a)
+}
+
+#[cfg(test)]
+mod tests {
+	use super::part_a;
+
+	use indoc::indoc;
+
+    const CASE: &str = indoc! {"
+        L68
+        L30
+        R48
+        L5
+        R60
+        L55
+        L1
+        L99
+        R14
+        L82
+    "};
+
+	#[test]
+	fn part_a_test() {
+		assert_eq!(part_a(&CASE), 3);
+	}
 }
